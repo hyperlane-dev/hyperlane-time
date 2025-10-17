ecee79dc19ca1512711f35a8f92ac9df6a0424e5
尤雨东 <root@ltpp.vip>
2025-10-17 21:23:32 +0800

    feat: v0.7.9

--- src/time/fn.rs
+++ src/time/fn.rs
@@ -20,6 +20,7 @@ pub const MONTHS: [&str; 12] = [
 /// # Returns
 ///
 /// - `bool` - Whether the year is a leap year.
+#[inline]
 pub fn is_leap_year(year: u64) -> bool {
     (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)
 }
